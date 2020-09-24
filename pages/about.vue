1332467b22c3209e8289907f1363f77cccdf08b4
Carl Anthony Handy <carl@intellectstorm.com>
2020-09-24 13:15:39 -0400

    Create about page

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+        <p>This is the nuxt js fundamentals series by Carl Handy. Learn more at <a href="https://kalpa.dev">
+        https://kalpa.dev</a></p>
+    </div>
+</template>
