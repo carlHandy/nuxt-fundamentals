6f01395a6c9e4eeab2e02c01ab99f431cfcfc0f2
Carl Anthony Handy <carl@intellectstorm.com>
2020-09-24 13:15:27 -0400

    Create hello world component

---
+++ components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+<div id="root">
+    <h1>Hello, World</h1>
+    <div class="links">
+        <nuxt-link to="/about"
+          class="button--green"
+        >
+         About
+        </nuxt-link>
+      </div>
+</div>
+</template>
